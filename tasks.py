fabb7f6b215a73700771c523ee952bbca075f0b5
Charlie <72133888+Catcatcher33@users.noreply.github.com>
2022-06-22 12:35:27 +0100

    add docstrings for task.py

--- tasks.py
+++ tasks.py
@@ -1,11 +1,14 @@
 from random import randint
 from itertools import combinations
 
-from grid import generate_grid, Grid
+from grid import generate_grid
 from path import compute_shortest_path
 
 
 def alternate_paths(obstacles: list, removed_count: int, start_cell: tuple, end_cell: tuple):
+    '''
+    In the event that a path cannot be computed, remove a given number of obstacles to find the next possible shortest path.
+    '''
     best_path: list = [0 for _ in range(99)]
     best_removed_obstacles: list = []
     path_exists: bool = False
@@ -29,6 +32,9 @@ def alternate_paths(obstacles: list, removed_count: int, start_cell: tuple, end_
 
 
 def execute(obstacles: list):
+    '''
+    With a given list of obstacles, find the shortest possible path.
+    '''
     start_cell = (0,0)
     end_cell = (9,9)      
     path: list = compute_shortest_path(generate_grid(obstacles), start_cell, end_cell)
@@ -43,16 +49,22 @@ def execute(obstacles: list):
 
 
 def phase_1():
+    '''
+    Finds the shortest path for a grid with a predefined set of obstacles.
+    '''
     print('==== Phase 1 ====')
     obstacles: list = [(7,7), (7,8), (8, 7), (9,7)]
     execute(obstacles)
 
 
 def phase_2():
+    ''' 
+    Finds the shortest path for a grid with Phase 1's obstacles and an additional random 20 obstacles.
+    '''
     print('==== Phase 2 ====')
 
     #  Create 20 individual obstacle coordinates
-    obstacles: list = []
+    obstacles: list = [(7,7), (7,8), (8, 7), (9,7)]
     i: int = 0
     while i < 20:
         x: int = randint(0, 9)
@@ -67,6 +79,10 @@ def phase_2():
 
 
 def bonus():
+    '''
+    For a set of obstacles where a path cannot be found, suggests the smallest possible 
+    reduced set of obstacles for a path to be computed.
+    '''
     print('==== Bonus ====')
     obstacles: list = [(8,9), (9,8), (8,8), (7,7), (7,8), (8,7), (7,9), (9,7)]
     execute(obstacles)
